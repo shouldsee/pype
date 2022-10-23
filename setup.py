deadcbf9b114d603d98fdec68e490dabb54ef120
shouldsee <shouldsee.gem@gmail.com>
2022-10-24 04:04:28 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#from setuptools import setup
+
+# from distutils.core import setup
+
+from distutils.core import setup
+import os,glob,sys
+config = dict(
+	name='pype',
+	version = '0.0.1',
+    packages=['pype'],
+	include_package_data=True,
+	license='MIT',
+	author='Feng Geng',
+	author_email='shouldsee@qq.com',
+	long_description=open('README.md').read(),
+	# python_requires = '>=3.6',
+	classifiers = [
+	'Programming Language :: Python :: 3.5',
+	'Programming Language :: Python :: 3.7',
+	],
+	install_requires=[
+	'toml',
+		# x.strip() for x in open("requirements.txt","r")
+        # 	if x.strip() and not x.strip().startswith("#")
+	],
+    entry_points={
+        "console_scripts": [
+            # "pyluck=luck.cli:luck_main",
+            # "pyluckmake=luck.luck_build_main:luck_build_main",
+            ]},
+
+)
+
+	# assert sys.version_info >= (3,5),('Requires python>=3.5, found python==%s'%('.'.join([str(x) for x in sys.version_info[:3]])))
+setup(**config)
+
+# if __name__ == '__main__':
